3cbad0bfcea0eb4fc20c50cb0cef2464a6c00145
Steijn <433269@student.fontys.nl>
2024-01-15 15:19:31 +0100

    Azure database

---
+++ BackendApp/BackendApp/Migrations/20240115141633_firstlaptop.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BackendApp.Migrations
+{
+    public partial class firstlaptop : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
