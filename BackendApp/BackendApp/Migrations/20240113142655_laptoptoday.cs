ff5540c719a91bd6cf3af7e33cef90cd8c614a47
Steijn <433269@student.fontys.nl>
2024-01-15 14:57:08 +0100

    cisco changes

---
+++ BackendApp/BackendApp/Migrations/20240113142655_laptoptoday.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BackendApp.Migrations
+{
+    public partial class laptoptoday : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
